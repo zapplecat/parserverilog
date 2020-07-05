65cb0f99fab38629b6aa2b149d7819d1fb1b515a
Brian Wu <poenbwu@gmail.com>
2020-07-04 22:14:21 -0700

    add terms and binds

--- parseverilog/processor.py
+++ parseverilog/processor.py
@@ -12,14 +12,24 @@ def main():
 
     analysis = VerilogDataflowAnalyzer(
         sample_files,
-        top_module=TOP_MODULE_NAME)  # Default is 'TOP'
+        topmodule=TOP_MODULE_NAME)  # Default is 'TOP'
 
     # Needed to generate analysis
     analysis.generate()
 
     # Getting Terms for each item
+    print('\n\nTERMS')
+    terms_map = analysis.getTerms()
+    term_list = terms_map.values()
+    for term in term_list:
+        print(term.tostr())
 
     # Getting Bind for each item
+    print('\n\nBINDS')
+    binds_map = analysis.getBinddict()
+    bind_list = binds_map.values()
+    for bind in bind_list:
+        print(bind[0].tostr())
 
 
 if __name__ == '__main__':
